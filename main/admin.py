bc3d7a34ef4ab965c59c91ab1ba467a652e6f57e
amir199675 <amirderakhshanfar251@gmail.com>
2020-01-15 13:45:42 +0330

    admin_panel

--- main/admin.py
+++ main/admin.py
@@ -3,9 +3,13 @@ from .models import *
 # Register your models here.
 
 
+class MyUserAdmin(admin.ModelAdmin):
+	search_fields = ('national_number', 'phone_number','first_name','last_name','is_staff')
+	list_display= ('__str__', 'national_number', 'phone_number','is_staff')
+
 admin.site.register(New)
 admin.site.register(Hadith)
 admin.site.register(Slider)
-admin.site.register(MyUser)
+admin.site.register(MyUser,MyUserAdmin)
 admin.site.register(Message)
 
